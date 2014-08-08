1716f3dc07fc7c4602d709f38432efe9b244d0da
Thomas Cabral <thethc96@yahoo.com>
2014-08-07 23:19:34 -0400

    first real commit

--- social_company/settings.py
+++ social_company/settings.py
@@ -18,9 +18,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 SECRET_KEY = 'epq6@#72mxvt=6g+8&186f$*-t8tg0bg-liaukj-%0w5z)a8js'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 # Application definition
 
@@ -78,10 +78,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '', '../templates').replace('\\','/'),)
